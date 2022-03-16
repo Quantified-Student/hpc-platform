f7bc1d653109d1e5fd348d84e22fd721c08113a3
Neal Geilen <info@nealgeilen.nl>
2022-03-16 14:45:34 +0100

    added first migration

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,25 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient } from '@prisma/client'
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
 ) {
 
-  const prisma = new PrismaClient()
+    const prisma = new PrismaClient()
 
-  async function main() {
-    return await prisma.user.findMany()
-  }
-
-  main()
-      .then((response) => {
-        res.status(200).json(response)
-      })
-      .catch((e) => {
-        throw e
-      })
-      .finally(async () => {
-        await prisma.$disconnect()
-      })
+    async function main() {
+        return await prisma.user.findMany()
+    }
 }
