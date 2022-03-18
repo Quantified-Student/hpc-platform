975ed687affd7817a035e3346e448c3d7fb4d70f
Jasper123Pyah <jaspermeiracker@gmail.com>
2022-03-18 12:53:55 +0100

    feature: login page skeleton

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,35 @@
+import {NextPage} from "next";
+import Head from "next/head";
+import {Grid, GridItem} from "@chakra-ui/react";
+import Header from "../../components/header";
+import SideMenu from "../../components/sidemenu";
+import styles from "./Dashboard.module.css";
+const Dashboard : NextPage = () => {
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>HPC</title>
+                <meta name="description" content="HPC platform"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <Header/>
+            <Grid
+                h={'100vh'}
+                templateRows='repeat(1, 1fr)'
+                templateColumns='repeat(5, 1fr)'
+                gap={4}
+            >
+                <GridItem colSpan={1}>
+                    <SideMenu/>
+                </GridItem>
+                <GridItem colSpan={4} bg='grey'> Content </GridItem>
+            </Grid>
+
+            <footer className={styles.footer}>
+                Footer
+            </footer>
+        </div>
+    )
+}
+export default Dashboard
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,55 @@
-import type {NextPage} from 'next'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import SideMenu from "./components/sidemenu";
-import {Grid, GridItem} from "@chakra-ui/react";
-import Header from "./components/header";
+import type {NextPage} from 'next';
+import {Box, Button, Grid, GridItem, Heading, Icon, List, ListItem} from "@chakra-ui/react";
+import styles from "../styles/Home.module.css";
+import {MdConnectWithoutContact, MdLogin, MdVpnKey} from "react-icons/all";
 
 const Home: NextPage = () => {
     return (
-        <div className={styles.container}>
-            <Head>
-                <title>HPC</title>
-                <meta name="description" content="HPC platform"/>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
-            <Header/>
+        <div>
             <Grid
                 h={'100vh'}
                 templateRows='repeat(1, 1fr)'
-                templateColumns='repeat(5, 1fr)'
-                gap={4}
+                templateColumns='repeat(2, 1fr)'
             >
                 <GridItem colSpan={1}>
-                    <SideMenu/>
+                    <div className={styles.container}>
+                        <Heading fontSize={'6vh'}>
+                            Welcome to GLaDOS
+                        </Heading>
+                        <Box bg='#663366' w='100%' minH='80%' color='white'>
+                            This is a cool image or animation or something idk
+                        </Box>
+                    </div>
                 </GridItem>
-                <GridItem colSpan={4} bg='grey'> Content </GridItem>
+                <GridItem bg='#663366'>
+                    <div className={styles.container}>
+                        <List spacing={5}>
+                            <ListItem fontSize={'4.5vh'}>
+                                <div className={styles.inlineCenter}>
+                                    <Icon as={MdLogin} m={'5px 5px 0 0'} color='#00c9c5'/>
+                                    Log in with your Fontys account.
+                                </div>
+                            </ListItem>
+                            <ListItem fontSize={'4.5vh'}>
+                                <div className={styles.inlineCenter}>
+                                    <Icon as={MdVpnKey} m={'5px 5px 0 0'} color='#00c9c5'/>
+                                    Create an SSH Key.
+                                </div>
+                            </ListItem>
+                            <ListItem fontSize={'4.5vh'}>
+                                <div className={styles.inlineCenter}>
+                                    <Icon as={MdConnectWithoutContact} m={'5px 5px 0 0'} color='#00c9c5'/>
+                                    Connect to me!
+                                </div>
+                            </ListItem>
+                        </List>
+                        <Button colorScheme='white' margin={'3vh'} size='lg' width={'80%'} height={'60px'}
+                                fontSize='3vh' variant={'outline'}>
+                            LOG IN
+                        </Button>
+                    </div>
+                </GridItem>
             </Grid>
-
-            <footer className={styles.footer}>
-                Footer
-            </footer>
         </div>
     )
 }
