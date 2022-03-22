ed13b505008935f134dc6d52f2a60e18dad6b26f
Jasper123Pyah <jaspermeiracker@gmail.com>
2022-03-22 22:55:00 +0100

    tweak: login page layout and buttoncolor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type {NextPage} from 'next';
-import {Box, Button, Grid, GridItem, Heading, Icon, List, ListItem} from "@chakra-ui/react";
+import {Box, Button, Grid, GridItem, Heading, List, ListItem} from "@chakra-ui/react";
 import styles from "../styles/Home.module.css";
 import {MdConnectWithoutContact, MdLogin, MdVpnKey} from "react-icons/md";
 
@@ -9,41 +9,41 @@ const Home: NextPage = () => {
             <Grid
                 h={'100vh'}
                 templateRows='repeat(1, 1fr)'
-                templateColumns='repeat(2, 1fr)'
+                templateColumns='repeat(3, 1fr)'
             >
-                <GridItem colSpan={1}>
-                    <div className={styles.container}>
-                        <Heading m={'3vh'} fontSize={'6vh'}>
+                <GridItem className={styles.sideBox} colSpan={1}>
+                    <div className={styles.leftContainer}>
+                        <Heading m={'3vh'} textAlign={'center'} fontSize={'7vh'}>
                             Welcome to GLaDOS
                         </Heading>
-                        <Box bg='#663366' w='100%' minH='80%' color='white'>
+                        <Box bg='#663366' w='100%' h='65%' color='white'>
                             This is a cool image or animation or something idk
                         </Box>
                     </div>
                 </GridItem>
-                <GridItem bg='#663366'>
-                    <div className={styles.container}>
-                        <List spacing={5}>
-                            <ListItem fontSize={'4.5vh'}>
+                <GridItem className={styles.sideBox} colSpan={2} bg='#663366'>
+                    <div className={styles.rightContainer}>
+                        <List marginLeft={'2%'} spacing={5}>
+                            <ListItem fontSize={'5vh'}>
                                 <div className={styles.inlineCenter}>
                                     <MdLogin className={styles.icon}/>
                                     Log in with your Fontys account.
                                 </div>
                             </ListItem>
-                            <ListItem fontSize={'4.5vh'}>
+                            <ListItem fontSize={'5vh'}>
                                 <div className={styles.inlineCenter}>
                                     <MdVpnKey className={styles.icon}/>
                                     Create an SSH Key.
                                 </div>
                             </ListItem>
-                            <ListItem fontSize={'4.5vh'}>
+                            <ListItem fontSize={'5vh'}>
                                 <div className={styles.inlineCenter}>
                                     <MdConnectWithoutContact className={styles.icon}/>
                                     Connect to me!
                                 </div>
                             </ListItem>
                         </List>
-                        <Button colorScheme='white' margin={'3vh'} size='lg' width={'80%'} height={'60px'}
+                        <Button borderColor={'#00c9c5'} margin={'3vh'} size='lg' border='4px' width={'80%'} height={'10vh'}
                                 fontSize='3vh' variant={'outline'}>
                             LOG IN
                         </Button>
